e4ffe8ee491a4c76b2453594e61b3129ef1e5063
Jin Lei <jin.lei@ubc.ca>
2020-05-24 00:37:12 -0700

    ssl

--- alphawrite/settings.py
+++ alphawrite/settings.py
@@ -114,6 +114,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+# SSL
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
