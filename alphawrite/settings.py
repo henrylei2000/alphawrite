2ed93d4cdf337122230b3396cfd12877062ad69d
Henry Lei <henrylei2000@gmail.com>
2020-08-10 20:46:47 -0700

    UI changes 2.0

--- alphawrite/settings.py
+++ alphawrite/settings.py
@@ -120,9 +120,6 @@ SECURE_SSL_REDIRECT = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-   ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
--- alphawrite/urls.py
+++ alphawrite/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('essay.urls')),
-    path('essay/', include('essay.urls')),
     path('admin/', admin.site.urls),
 ]
