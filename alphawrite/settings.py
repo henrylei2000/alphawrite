081575ff504cc3f707f4fa3b09748e662e40a4b5
Henry Lei <henrylei2000@gmail.com>
2020-08-10 20:58:57 -0700

    static

--- alphawrite/settings.py
+++ alphawrite/settings.py
@@ -123,4 +123,3 @@ SECURE_SSL_REDIRECT = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
